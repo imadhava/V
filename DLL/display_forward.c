6dad39ac69d698d73a0fda269b952a0d87acbd4f
I MR <119655122+imadhava@users.noreply.github.com>
2024-09-06 19:37:15 +0530

    Update display_forward.c

---
+++ DLL/display_forward.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+void displayForward(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    struct Node* node1 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* node2 = (struct Node*)malloc(sizeof(struct Node));
+    head = node1;
+    node1->data = 10;
+    node1->next = node2;
+    node1->prev = NULL;
+    node2->data = 20;
+    node2->next = NULL;
+    node2->prev = node1;
+
+    displayForward(head);
+    return 0;
+}
